3b8796276dd777544a15e960a34ac53c4363550c
Nick Ruider <nick.ruider@gmail.com>
2023-05-17 13:39:52 +0200

    Hot fix

--- lib/entities/list_item.dart
+++ lib/entities/list_item.dart
@@ -3,36 +3,29 @@ import 'dart:core';
 import 'package:flutter/foundation.dart';
 
 import 'dto/song_dto.dart';
-import 'enum/upload_state.dart';
 
 class ListItem {
-  String id;
   Map<String, dynamic> id3Tags;
   Uint8List bytes;
+  late bool isExpanded;
   late SongDTO song;
-  UploadState uploadState;
 
   ListItem({
-    required this.id,
     required this.id3Tags,
+    required originalTitle,
     required this.bytes,
-    String? originalTitle,
-    this.uploadState = UploadState.notStarted,
   }) {
+    isExpanded = false;
     String artist, title, album;
     if (id3Tags['Artist'] != null) {
       artist = id3Tags['Artist'];
-    } else if (originalTitle != null) {
-      artist = originalTitle.split(' – ').first;
     } else {
-      artist = "";
+      artist = originalTitle.split(' – ').first;
     }
     if (id3Tags['Title'] != null) {
       title = id3Tags['Title'];
-    } else if (originalTitle != null) {
-      title = originalTitle.split(' – ').last;
     } else {
-      title = "";
+      title = originalTitle.split(' – ').last;
     }
     if (id3Tags['Album'] != null) {
       album = id3Tags['Album'];
@@ -41,14 +34,5 @@ class ListItem {
     }
     song = SongDTO(artists: [artist], title: title, album: album);
   }
-
-  ListItem setSongDTO(SongDTO song) {
-    this.song = song;
-    return this;
-  }
-
-  ListItem setUploadState(UploadState uploadState) {
-    this.uploadState = uploadState;
-    return this;
-  }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'constants.dart';
 import 'firebase_options.dart';
@@ -13,7 +12,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await setupGetIt();
-  runApp(ProviderScope(child: SelfradioApp()));
+  runApp(SelfradioApp());
 }
 
 class SelfradioApp extends StatelessWidget {
